713d5e77f486067ba29362a524eea976b054cf60
roniery <roniery>
2024-06-03 11:03:37 -0300

    quarto commit

---
+++ routes/routes.js
@@ -0,0 +1,15 @@
+import express from 'express';
+const router = express.Router();
+import autenticacao from '../config/autenticacao.js';
+
+import passport from '../config/passport.js';
+
+import multer from 'multer';
+
+
+
+
+
+
+
+export default router;
